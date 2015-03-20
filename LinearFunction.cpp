e1e98594c6bcc6812d0b9157d8b9515dbe307ad6
Michał Gutowski <m.gutowski@binarapps.com>
2015-03-20 12:12:33 +0100

    [fixed] neuron

---
+++ LinearFunction.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Michał Gutowski on 20.03.15.
+//
+
+#include "LinearFunction.h"
---
+++ LinearFunction.h
@@ -0,0 +1,14 @@
+//
+// Created by Michał Gutowski on 20.03.15.
+//
+
+#ifndef _SOFT_COMPUTING_1_LINEARFUNCTION_H_
+#define _SOFT_COMPUTING_1_LINEARFUNCTION_H_
+
+
+class LinearFunction {
+
+};
+
+
+#endif //_SOFT_COMPUTING_1_LINEARFUNCTION_H_
